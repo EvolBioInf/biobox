f8be5728525be1b3fe1dfdcb92e3a9c5938a93c9
Bernhard Haubold <haubold@evolbio.mpg.de>
2021-09-15 17:39:14 +0200

    Stop tracking

--- scripts/date.sh
+++
@@ -1,4 +0,0 @@
-git log |
-    grep Date |
-    head -n 1 |
-    awk '{printf "%s_%s_%s_%s-%s\n", $2, $3, $4, $6, $5}'
--- scripts/version.sh
+++
@@ -1,4 +0,0 @@
-#git describe | sed -E 's/^[vV]//; s/(.)$/\1\\/; s/-g(.......)/\\ \\(\1\\)/' | tr -d '\n'
-git describe |
-    tr ' ' '_' |
-    tr -d '\n'
