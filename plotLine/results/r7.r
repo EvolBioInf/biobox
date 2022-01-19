e2995491c6d1292f82f351c528e38c9aff7f838a
Bernhard Haubold <haubold@evolbio.mpg.de>
2022-01-19 11:28:58 +0100

    Testing now based on R scripts
    It used to be based on the postscript files produced by R. But I noticed
    that these aren't constant across R versions. So I now allow printing of
    the R file and use that for testing.

---
+++ plotLine/results/r7.r
@@ -0,0 +1,19 @@
+library("ggplot2")
+data <- read.table(file="stdin")
+plot <- ggplot(data, aes(V1, V2, group=V3))
+
+plot <- plot + xlab("x")
+
+
+plot <- plot + ylab("y")
+
+
+
+plot <- plot + geom_path(aes(color=factor(V3)))
+
+plot <- plot + labs(color="")
+
+x11()
+plot(plot)
+while(names(dev.cur()) != 'null device')
+    Sys.sleep(0.1)
---
+++ plotLine/results/r8.r
@@ -0,0 +1,19 @@
+library("ggplot2")
+data <- read.table(file="stdin")
+plot <- ggplot(data, aes(V1, V2, group=V3))
+
+plot <- plot + xlab(NULL)
+
+
+plot <- plot + ylab(NULL)
+
+
+
+plot <- plot + geom_path(aes(color=factor(V3)))
+
+plot <- plot + labs(color="")
+
+x11(width=2.3622047244094486)
+plot(plot)
+while(names(dev.cur()) != 'null device')
+    Sys.sleep(0.1)
