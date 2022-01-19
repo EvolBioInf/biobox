84b5b83f748b8da160ef8c7ae4acd37d1ddd997f
Bernhard Haubold <haubold@evolbio.mpg.de>
2022-01-19 18:03:03 +0100

    Use testPlots.sh to test plotting
    The testing is interactive, so not part of the main testing mechanism.

---
+++ plotSeg/testPlots.sh
@@ -0,0 +1,10 @@
+./plotSeg test.dat
+./plotSeg -x "x" test.dat
+./plotSeg -y "y" test.dat
+./plotSeg -x "x" -y "y" test.dat
+./plotSeg -X 100:500 test.dat
+./plotSeg -Y 100:500 test.dat
+./plotSeg -X 100:500 -Y 100:500 test.dat
+./plotSeg -w 5 test.dat
+./plotSeg -H 6 test.dat
+./plotSeg -w 5 -H 6 test.dat
