4c0dfabf16b0dbbf73a07d80ab3ee6da9f83e3a8
Bernhard Haubold <haubold@evolbio.mpg.de>
2024-06-01 11:54:40 +0200

    First ensure sudo

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,10 @@
+s=$(which sudo)
+if [[ $s == "" ]]; then
+    apt update
+    apt -y install sudo
+fi
+h=$(history | tail | grep update)
+if [[ $h == "" ]]; then
+    sudo apt update
+fi
 sudo apt -y install gnuplot golang graphviz libdivsufsort-dev libgsl-dev make
